03130d4be5e80a062be7f8fb8e06b2f03ccbe73b
Sergey Tolmachev <tetron@gmail.com>
2018-05-11 18:12:02 +0300

    End of Lecture 2.

---
+++ Concentration/Concentration/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Sergey Tolmachev on 11/05/2018.
+//  Copyright © 2018 Sergey Tolmachev. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqIdentifier()
+    }
+}
